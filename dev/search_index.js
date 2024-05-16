daf7473fd425084857ebacbdd857af2a6f77c98c
Documenter.jl <documenter@juliadocs.github.io>
2024-05-16 02:23:09 +0000

    build based on 328b199

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TWGEMSDatasets","category":"page"},{"location":"#TWGEMSDatasets","page":"Home","title":"TWGEMSDatasets","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TWGEMSDatasets.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TWGEMSDatasets]","category":"page"}]
+}

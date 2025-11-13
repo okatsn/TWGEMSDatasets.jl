3afb15bf5448c2321dce0daf637412465edc7200
okatsn <okatsn@gmail.com>
2025-11-13 11:13:24 +0800

    Add `set_GE` and `set_GM`

--- src/stationtype.jl
+++ src/stationtype.jl
@@ -1,5 +1,51 @@
 const typestr_GE = "GE"
 const typestr_GM = "GM"
+const set_GE = Set(
+    "KUOL",
+    "HUAL",
+    "TOCH",
+    "ENAN",
+    "SIHU",
+    "HERM",
+    "CHCH",
+    "DAHU",
+    "KAOH",
+    "PULI",
+    "SHRL",
+    "SHCH",
+    "FENL",
+    "YULI",
+    "RUEY",
+    "LIOQ",
+    "LISH",
+    "DABA",
+    "WANL",
+    "FENG",
+    "HUZS",
+)
+
+const set_GM = Set(
+    "MS",
+    "TW",
+    "TT",
+    "YL",
+    "HC",
+    "HL",
+    "PT",
+    "YH",
+    "SL",
+    "LY",
+    "NC",
+    "KM",
+    "CS",
+    "MT",
+    "LN",
+    "ZB",
+    "XC",
+    "SM",
+    "CN",
+)
+
 
 const dict_code2type = Dict(
     "MS" => typestr_GM,
